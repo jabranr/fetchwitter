9e447bb22e6e148f151a62ed58e06c48e3936fb0
Jabran Rafique <hello@jabran.me>
2014-04-02 00:34:17 +0100

    update test cases

--- Tests/FetchwitterTest.php
+++ Tests/FetchwitterTest.php
@@ -1,28 +1,25 @@
 <?php
 
-include_once('../src/Fetchwitter.php');
-include_once('../vendor/autoload.php');
+require_once('./src/Fetchwitter.php');
+require_once('./vendor/autoload.php');
 
 class FetchwitterTest extends PHPUnit_Framework_TestCase {	
 	public $fetchwitter, $config;
 
 	public function setUp() {
 		$this->config = array(
-			'api_key' => 'INSERT_API_KEY_HERE',
-			'api_secret' => 'INSERT_API_SECRET_HERE'
+			'api_key' => 'hqXaOd33grIEIOizaUvpcQ',
+			'api_secret' => 'Kxs68MJGHvtTe9CArjLntsc9iVDEygIfqTJlrcqMhLM'
 		);
+		$this->fetchwitter = new Fetchwitter( $this->config );
 	}
 
 	public function tearDown() {
-		unset($this->fetchwitter);
-	}
-
-	public function testSetUpInstance() {
-		$this->fetchwitter = new FetchwitterTest( $this->config );
+		unset( $this->fetchwitter );
 	}
 
 	public function testConfigProvided() {
-		$this->assertTrue( isset($this->config) );
+		$this->assertTrue( isset( $this->config ) );
 	}
 
 	public function testAPIKeyIsSet() {
@@ -31,5 +28,24 @@ class FetchwitterTest extends PHPUnit_Framework_TestCase {
 
 	public function testAPISecretIsSet() {
 		$this->assertArrayHasKey( 'api_secret', $this->config );
+	}
+
+	/**
+	 * @dataProvider testCaseTweetsForToTweetMethod
+	 */
+
+	public function testToTweet($staticTweet, $formattedTweet) {
+		$resultTweet = $this->fetchwitter->to_tweet( $staticTweet );
+		$this->assertEquals($resultTweet, $formattedTweet);
+	}
+
+	public function testCaseTweetsForToTweetMethod() {
+		return array(
+			array('test tweet', 'test tweet')
+			// array('#Test #tweet', '<a target="_blank" href="https://twitter.com/search?q=%23Test" ref="nofollow">#Test</a> <a target="_blank" href="https://twitter.com/search?q=%23tweet" ref="nofollow">#tweet</a>')
+			// array('@Test @tweet', '<a target="_blank" href="https://twitter.com/Test" ref="nofollow">@Test</a> <a href="https://twitter.com/tweet" ref="nofollow">@tweet</a>'),
+			// array('@Test #tweet', '<a target="_blank" href="https://twitter.com/Test" ref="nofollow">@Test</a> <a href="https://twitter.com/search?q=%23tweet" ref="nofollow">#tweet</a>'),
+			// array('Test tweet at http://example.com', 'Test tweet at <a target="_blank" href="http://example.com" ref="nofollow">http://example.com</a>')
+		);
 	}
 }
